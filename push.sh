c6e964d90bb3edfb49c3d0ba4a4a2e7aa6434d2a
root <root@ip-172-31-23-109.eu-north-1.compute.internal>
2026-02-18 08:12:13 +0000

    nothing changed just writed some lines

--- push.sh
+++ push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-# Stop if any command fails
+G# Stop if any command fails
 set -e
 
 # Check if commit message is provided
@@ -19,4 +18,5 @@ git commit -m "$1"
 echo "🚀 Pushing to GitHub..."
 git push
 
-echo "✅ Done!"
+echo "✅ Done!" 
+i was changeing this 
